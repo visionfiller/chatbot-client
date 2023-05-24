bf6f93c69c4644cfd057f8bf8980609eff3ef9d7
Vision Filler <visionfiller@gmail.com>
2023-05-24 09:08:07 -0500

    working

--- src/App.js
+++ src/App.js
@@ -5,58 +5,72 @@ const data = {
   headerText: "Hello hello ✨",
   pText: "I'm a cute chatbot!",
   p2Text: "I can help you with your horoscope",
-  conversation: [
-    { role: "assistant", content: "Hello, how can I help you today?" },
-    { role: "user", content: "I need a horoscope reading" },
-  ],
+  conversation: [],
   isLoading: false
 };
 
 function App() {
   const [conversation, setConversation] = React.useState(data.conversation);
   const [isLoading, setIsLoading] = React.useState(data.isLoading);
- 
- 
-
-
+  const [loadingConvo, setLoadingConvo] = React.useState([])
 
   const updateUserMessages = (newMessage) => {
     if (!newMessage) {
       return;
     }
     setIsLoading(true) //we set loading to true here to display the balls in the assistant message bubble while we wait for the response from the server
-    const newConversation =[
+    const newConversation = [
       ...conversation,
       { role: "user", content: newMessage }
-    ];
+    ]
+    // const loadingConversation = [
+    //   ...newConversation,
+    //   { role: "assistant", content: "Loading" }
+    // ]
 
     // send POST request to local server with the conversation payload for chat response 
     // "http://localhost:8088/chat", {//i will fill in this function later
     // it is necessary to temporarily use 'loading' as a message until we get a reponse from the server if we want to display the ellipses 
-    setConversation = [...newConversation]
+    setConversation([
+      ...newConversation
+    ])
+    setLoadingConvo([
+      ...newConversation,
+      { role: "assistant", content: "Loading" }
+    ])
 
+    // setConversation(loadingConversation)
 
-    
-    //then i will set the assistant message in this conversation with the actual response from the assistant  
-    fetch("http://localhost:8088/chat", {
+    fetch('http://localhost:8088/chat', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(newConversation)
     }).then(res => res.json())
-      .then((response) =>
-        setConversation([
-          ...newConversation,
-          { role: "assistant", content: response }
-        ]))
+      .then((response) => setConversation([
+        ...newConversation,
+        { role: "assistant", content: response }
+      ])).then(() => setIsLoading(false))
 
+
+
+    //then i will set the assistant message in this conversation with the actual response from the assistant  
 
 
 
   };
-
   const showMessages = () => {
+    if (isLoading) {
+      return loadingConvo.map((message, index) => (
+        <MessageBubble
+          key={index}
+          message={message.content}
+          role={message.role}
+          isLoading={isLoading}
+        />
+      ))
+    }
     return conversation.map((message, index) => (
       <MessageBubble
         key={index}
@@ -68,7 +82,7 @@ function App() {
   };
 
   const onInput = (event) => {
-    if (event.key === "Enter") {
+    if (event.key === "Enter" && isLoading === false) {
       const userInput = event.target.value;
 
       updateUserMessages(userInput);
@@ -77,11 +91,12 @@ function App() {
   };
 
   const onClick = () => {
-    const inp = document.getElementById("chat");
-    const userInput = inp.value;
-
-    updateUserMessages(userInput);
-    inp.value = "";
+    if (isLoading === false) {
+      const inp = document.getElementById("chat");
+      const userInput = inp.value;
+      updateUserMessages(userInput);
+      inp.value = "";
+    }
   };
 
   return (
@@ -102,7 +117,7 @@ function App() {
 
 function MessageBubble(props) {
   const messageClass = props.role === "user" ? "user" : "assistant";
-
+  //if is loading from props
   return (
     <div className={`message-container ${messageClass}-message-container`}>
       {props.role === "assistant" && props.isLoading && props.message === 'Loading' ? (
